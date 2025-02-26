46959472e5f6aad1a01764273eaf92b22951e445
htft1e22 <htft1e22@soton.ac.uk>
2025-02-26 16:19:09 +0000

    Calculations
    * Setters and Getters for Obstacles, base calculation with final RESA and obstacle getters
    
    * Added Setters and Getters to runways, set current values to default values

--- src/main/java/uk/ac/soton/group2seg/model/Obstacle.java
+++ src/main/java/uk/ac/soton/group2seg/model/Obstacle.java
@@ -19,4 +19,22 @@ public class Obstacle {
     this.distLeftThreshold = distLeftThreshold;
     this.centreOffset = centreOffset;
   }
+  public int getHeight() {
+    return height;
+  }
+  public void setHeight(int height) {
+    this.height = height;
+  }
+  public int getWidth() {
+    return width;
+  }
+  public void setWidth(int width) {
+    this.width = width;
+  }
+  public int getDistLeftThreshold() {
+    return distLeftThreshold;
+  }
+  public int getCentreOffset() {
+    return centreOffset;
+  }
 }
