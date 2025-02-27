cf21e5ae5cb9003c984e32d006246f17ad5c9569
lmt1n22 <lmt1n22@soton.ac.uk>
2025-02-27 15:22:36 +0000

    Validating model object implementations
    * Implementing airport methods and validating initialisation of objects
    
    * Testing scenebuilder and FXML files

--- src/main/java/uk/ac/soton/group2seg/model/testing.java
+++ src/main/java/uk/ac/soton/group2seg/model/testing.java
@@ -1,6 +1,8 @@
 package uk.ac.soton.group2seg.model;
 
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import uk.ac.soton.group2seg.model.utility.JaxbUtility;
 
 public class testing {
@@ -16,6 +18,20 @@ public class testing {
       System.out.println(airportList.getList());
 
       System.out.println(airport.getName());
+
+      ArrayList<Runway> runways = airport.getRunways();
+
+      for(Runway runway : runways) {
+        LogicalRunway lowerRunway = runway.getLowerRunway();
+        lowerRunway.initialise();
+        LogicalRunway higherRunway = runway.getHigherRunway();
+        higherRunway.initialise();
+
+        System.out.println(runway.getName());
+        System.out.println(lowerRunway.getDistances());
+        System.out.println(higherRunway.getDistances());
+      }
+
     }catch (Exception e){
       e.printStackTrace();
       System.out.println(e.getMessage());
