d85aec6313c75c53c69f8f45167f139e1d4cb826
htft1e22 <htft1e22@soton.ac.uk>
2025-03-14 17:18:06 +0000

    -
    * Missing import
    
    * Calculation logging to file + Base css for log in / register forms
    
    * Nottifications
    
    * Notification for incorrect obstacle inputs
    
    * Password hashing implemented with checking passowrd
    
    * Login pagd modifications, connected to main view, and made it look better
    
    * made all breakdown messages complete
    
    * Added calcualtion breakdowns fully
    
    * Replaced printing with logging
    
    * Added calculation breakdown area to GUI
    
    * Fixed logging, now logs in console and creates log file.
    
    * All fields show correctly when first running app
    
    * removed extension
    
    * Fixed LDA Calculations
    
    * -
    
    * -
    
    * Added function to update table once obstacle is placed
    
    * Finalizing calculator
    
    * Renamed window title
    
    * Added labels to runway value tables and made sure all table coloumns show
    
    * get current runway in airport
    
    * -
    
    * added one method which has all runway details getters, will be modified later
    
    * Added 2 tables on right details with coloum of values for runways
    
    * movied Views file to resources, App now opens RunwayView.fxml
    
    * Added code to launch RunwayView.fxml
    
    * fixed errors
    
    * -
    
    * added scenarios for calculations
    
    * made return threshold always return calculated threshold not defined one

--- src/main/java/uk/ac/soton/group2seg/controller/PredefinedObstacleController.java
+++ src/main/java/uk/ac/soton/group2seg/controller/PredefinedObstacleController.java
@@ -2,6 +2,7 @@ package uk.ac.soton.group2seg.controller;
 
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
