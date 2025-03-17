6a8dd66a42aee5af0850f0e71af3bc153db6de8a
htft1e22 <htft1e22@soton.ac.uk>
2025-03-17 18:35:09 +0000

    Testing
    * Calculator JUnit Testing
    
    * Fixed text fields placement
    
    * Use of Stylesheet for Forms
    
    * Obstacle Form Modifications
    
    * Remodeled Predefined Obstacle Form
    
    * Added adding airport forms with storing to XML and Database.
    
    * Fixed multiple airport runway showing
    
    * Opens log in view in start
    
    * Modification to runway view to make it look better
    
    * Missing import
    
    * Calculation logging to file + Base css for log in / register forms
    
    * Nottifications
    
    * Notification for incorrect obstacle inputs
    
    * Password hashing implemented with checking passowrd
    
    * Login pagd modifications, connected to main view, and made it look better
    
    * made all breakdown messages complete
    
    * Added calcualtion breakdowns fully
    
    * Replaced printing with logging
    
    * Added calculation breakdown area to GUI
    
    * Fixed logging, now logs in console and creates log file.
    
    * All fields show correctly when first running app
    
    * removed extension
    
    * Fixed LDA Calculations
    
    * -
    
    * -
    
    * Added function to update table once obstacle is placed
    
    * Finalizing calculator
    
    * Renamed window title
    
    * Added labels to runway value tables and made sure all table coloumns show
    
    * get current runway in airport
    
    * -
    
    * added one method which has all runway details getters, will be modified later
    
    * Added 2 tables on right details with coloum of values for runways
    
    * movied Views file to resources, App now opens RunwayView.fxml
    
    * Added code to launch RunwayView.fxml
    
    * fixed errors
    
    * -
    
    * added scenarios for calculations
    
    * made return threshold always return calculated threshold not defined one

---
+++ src/test/java/uk/ac/soton/group2seg/CalculatorTest.java
@@ -0,0 +1,64 @@
+package uk.ac.soton.group2seg;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import uk.ac.soton.group2seg.controller.Calculator;
+import uk.ac.soton.group2seg.model.LogicalRunway;
+import uk.ac.soton.group2seg.model.Obstacle;
+import uk.ac.soton.group2seg.model.Runway;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CalculatorTest {
+    private Runway runway09L27R;
+    private Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        LogicalRunway lowerRunway09L = new LogicalRunway("09L", 3902, 3902, 3902, 3595);
+        LogicalRunway higherRunway27R = new LogicalRunway("27R", 3884, 3962, 3884, 3884);
+
+        runway09L27R = new Runway(lowerRunway09L, higherRunway27R);
+        calculator = new Calculator(runway09L27R);
+    }
+
+    @Test
+    void testLandingOverObstacle() {
+        Obstacle obstacle = new Obstacle(25, 3384, 500, 0);
+        calculator.redeclareRunway(obstacle);
+        int newLDA = runway09L27R.getHigherRunway().getCurrLda();
+        assertEquals(2074, newLDA);
+    }
+
+    @Test
+    void testLandingTowardsObstacle() {
+        Obstacle obstacle = new Obstacle(0, 2600, 1302, 0);
+        calculator.redeclareRunway(obstacle);
+        int newLDA = runway09L27R.getLowerRunway().getCurrLda();
+        assertEquals(2300, newLDA);
+
+    }
+
+    @Test
+    void testTakingOffTowardsObstacle() {
+        Obstacle obstacle = new Obstacle(25, 2500, 1402, 0);
+        calculator.redeclareRunway(obstacle);
+        int newTORA = runway09L27R.getLowerRunway().getCurrTora();
+        // Test fails due to displaced threshold being calculated differently to what's in the project definition
+        assertEquals(1496, newTORA);
+        assertEquals(1496, runway09L27R.getLowerRunway().getCurrAsda());
+        assertEquals(1496, runway09L27R.getLowerRunway().getCurrToda());
+
+    }
+
+    @Test
+    void testTakingOffAwayFromObstacle() {
+        Obstacle obstacle = new Obstacle(0, 3384, 500, 0); // Distance from 27R threshold
+        calculator.redeclareRunway(obstacle);
+        int newTORA = runway09L27R.getHigherRunway().getCurrTora();
+        assertEquals(3084, newTORA);
+        assertEquals(3162, runway09L27R.getHigherRunway().getCurrToda()); // TODA = 3962 - 500 - 300 = 3162m
+        assertEquals(3084, runway09L27R.getHigherRunway().getCurrAsda()); // ASDA = 3884 - 500 - 300 = 3084m
+
+    }
+}
